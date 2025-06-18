a5b88d26a17947534d0d0b9d6942c3f510622057
hugo-chilemme <hugo.chilemme@gmail.com>
2025-06-18 22:32:56 +0200

    feat: add MarketingLayout component to manage layout structure for marketing pages

---
+++ frontend/app/(public)/layout.tsx
@@ -0,0 +1,14 @@
+
+interface MarketingLayoutProps {
+  children: React.ReactNode;
+}
+
+export default async function MarketingLayout({
+  children,
+}: MarketingLayoutProps) {
+  return (
+    <>
+      <main className="mx-auto flex-1 overflow-hidden">{children}</main>
+    </>
+  );
+}

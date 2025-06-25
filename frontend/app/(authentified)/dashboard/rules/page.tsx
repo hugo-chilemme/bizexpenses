2b79d16130edb5bbe7942b8bf16b3d2f034786a6
hugo-chilemme <hugo.chilemme@gmail.com>
2025-06-25 22:04:10 +0200

    feat: add rules page for administrators and HR managers

---
+++ frontend/app/(authentified)/dashboard/rules/page.tsx
@@ -0,0 +1,12 @@
+
+
+
+export default function Page() {
+	return (
+		<div className="flex flex-col items-center justify-center h-full">
+			<h1 className="text-2xl font-bold mb-4">Page des règles</h1>
+			<p>Cette page est réservée aux administrateurs et aux responsables RH.</p>
+			<p>Veuillez contacter votre administrateur pour plus d'informations.</p>
+		</div>
+	);
+}

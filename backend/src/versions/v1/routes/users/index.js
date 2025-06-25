bf8e2e3107ec481a4489398ce4deebc914a9b0cf
hugo-chilemme <hugo.chilemme@gmail.com>
2025-06-25 22:02:40 +0200

    feat: update user roles and enhance user deletion logic in user routes

--- backend/src/versions/v1/routes/users/index.js
+++ backend/src/versions/v1/routes/users/index.js
@@ -1,38 +1,40 @@
 const database = require('../../utils/database');
+const mailCreator = require('../../utils/mailCreator')
 
 const config = {
     get: {
         isAuthentified: true,
+        _entreprise_roles: ['owner', 'hr'],
     },
     post: {
         isAuthentified: true,
-        _entreprise_roles: ['owner', 'rh'],
+        _entreprise_roles: ['owner', 'hr'],
+    },
+    delete: {
+        isAuthentified: true,
+        _entreprise_roles: ['owner', 'hr'],
     }
 };
 
 const get = async (req, res) => {
-    const role = req.user?.entreprise?.role;
-    if (role === "owner" || role === "rh") {
-        // Fetch all employees
-        try {
-            const entreprise = await database().collection('entreprises').findOne({ "uuid": req.user.entreprise.uuid });
+    try {
+        const entreprise = await database().collection('entreprises').findOne({ "uuid": req.user.entreprise.uuid });
 
-            for (const user of entreprise.users) {
-                const uid = await database().collection('users').findOne({ uuid: user.id });
-                if (uid) {
-                    user.firstName = uid.firstName;
-                    user.lastName = uid.lastName;
-                    user.email = uid.email;
-                    user.active = uid.active;
-                }
+        for (const user of entreprise.users) {
+            const uid = await database().collection('users').findOne({ uuid: user.id });
+            if (uid) {
+                user.firstName = uid.firstName;
+                user.lastName = uid.lastName;
+                user.email = uid.email;
+                user.active = uid.active;
             }
-
-
-            return res.status(200).json({ status: 'success', data: entreprise.users });
-        } catch (err) {
-            console.error('Error fetching employees:', err);
-            return res.status(500).json({ status: 'error', message: 'Failed to fetch employees' });
         }
+
+
+        return res.status(200).json({ status: 'success', data: entreprise.users });
+    } catch (err) {
+        console.error('Error fetching employees:', err);
+        return res.status(500).json({ status: 'error', message: 'Failed to fetch employees' });
     }
     // Default: return only the current user
     return res.status(200).json({ status: 'success', data: req.user });
@@ -50,6 +52,7 @@ const post = async (req, res) => {
         return res.status(404).json({ status: 'error', message: 'Entreprise not found' });
     }
 
+    const checkExist = await database().collection('users').findOne({ email: email });
     const user = {
         uuid: require('uuid').v4().replace(/-/g, ''),
         firstName,
@@ -57,18 +60,58 @@ const post = async (req, res) => {
         email,
         role,
         active: true,
-        regisered: false,
+        reset_token: require('crypto').randomBytes(64).toString('hex'),
         createdAt: new Date(),
         updatedAt: new Date()
     };
 
-    await database().collection('users').insertOne(user);
+    if (!checkExist) {
+
+        await database().collection('users').insertOne(user);
+    }
+
+    const mail = new mailCreator();
+    mail.setSubject(`You're invited to join ${entreprise.name}`);
+    mail.setText(`Hello ${firstName} ${lastName},\n\nYou have been invited to join the entreprise ${entreprise.name}.\nPlease follow the link to complete your registration: https://bizexpenses.hugochilemme.com/set-auth/${user.reset_token}\n\nBest regards,\nThe BizExpenses Team`);
+    mail.setRecipients(email);
+    mail.send();
+
     await database().collection('entreprises').updateOne(
         { "uuid": req.user.entreprise.uuid },
-        { $push: { users: { id: user.uuid } } }
+        { $push: { users: { id: user.uuid, role: "user" } } }
     );
 
     return res.status(201).json({ status: 'success', data: user });
 };
 
-module.exports = { get, post, config };
+const del = async (req, res) => {
+    const userId = req.query.userId;
+    if (!userId) {
+        return res.status(400).json({ status: 'error', message: 'User ID is required' });
+    }
+
+    const entrepriseUuid = req.user.entreprise.uuid;
+
+    // Check if the user exists
+    const user = await database().collection('users').findOne({ uuid: userId });
+    if (!user) {
+        return res.status(404).json({ status: 'error', message: 'User not found' });
+    }
+
+    // Check if the user is part of the entreprise
+    const entreprise = await database().collection('entreprises').findOne({ uuid: entrepriseUuid });
+    if (!entreprise || !entreprise.users.some(u => u.id === userId))
+    {
+        return res.status(404).json({ status: 'error', message: 'User not part of the entreprise' });
+    }   
+
+    // Remove the user from the entreprise
+    await database().collection('entreprises').updateOne(
+        { uuid: entrepriseUuid },
+        { $pull: { users: { id: userId } } }
+    );  
+
+    res.status(200).json({ status: 'success', message: 'User deleted successfully' });
+};        
+
+module.exports = { get, post, config, delete: del };

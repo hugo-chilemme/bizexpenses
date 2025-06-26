1a9c731ecbf3d6c4d907bec6593d43a699e47d80
hugo-chilemme <hugo.chilemme@gmail.com>
2025-06-26 21:43:13 +0200

    fix: improve layout and alignment in BlockLanding component

--- frontend/app/(public)/(default)/blocks/BlockLanding.tsx
+++ frontend/app/(public)/(default)/blocks/BlockLanding.tsx
@@ -1,18 +1,19 @@
 "use client";
 import { useEffect } from "react";
 import { useAnimation, motion } from "framer-motion";
+import { isMobile } from "@/lib/utils";
 
 
 export default function BlockLanding({rules}) {
   return (
-<div className="2xl:w-[70vw] min-h-screen  mx-auto relative flex flex-col md:flex-row items-center justify-between">
+<div className="2xl:w-[70vw] min-h-screen mx-auto relative flex flex-col md:flex-row items-center justify-between">
         {/* Texte à gauche */}
-        <div className="flex-1 flex flex-col justify-center items-start">
+        <div className="flex-1 flex flex-col justify-center items-center md:items-start">
           <motion.h1
             initial={{ opacity: 0, x: -40 }}
             animate={{ opacity: 1, x: 0 }}
             transition={{ duration: 0.8 }}
-            className="text-5xl md:text-6xl font-black text-white mb-6 text-left"
+            className="text-5xl md:text-6xl font-black text-white mb-6 text-center md:text-left"
           >
             Simplifiez la gestion des notes de frais
           </motion.h1>
@@ -20,7 +21,7 @@ export default function BlockLanding({rules}) {
             initial={{ opacity: 0, x: -40 }}
             animate={{ opacity: 1, x: 0 }}
             transition={{ duration: 0.8, delay: 0.3 }}
-            className="text-lg md:text-2xl text-white/90 mb-10 text-left"
+            className="text-lg md:text-2xl text-white/90 mb-10 text-center md:text-left"
           >
             Désormais, gérez les dépenses de vos salariés en toute simplicité avec notre application de gestion des notes de frais.
           </motion.p>
@@ -42,8 +43,9 @@ export default function BlockLanding({rules}) {
 
           </div>
         </div>
-        <div className="flex-1 z-10 relative flex items-end justify-end">
-        </div>
+       { !isMobile() && (
+         <div className="flex-1 z-10 relative flex items-end justify-end"/>
+       )}
       </div>
   );
 }
